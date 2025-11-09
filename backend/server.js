637c605d664ba30ee1c106d44732f1e66d661a78
Mohit <mohittt0011@gmail.com>
2025-11-09 22:40:37 +0530

    Updated code / bug fixes / new features

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,7 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors({
-    origin: ['https://matify-frontend.vercel.app'],  // your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }))
+app.use(cors())
 
 // api endpoints
 app.use('/api/user',userRouter)
