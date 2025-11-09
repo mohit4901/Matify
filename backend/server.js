2bbe52066d9c110a3e0e54765d7356785426675a
Mohit <mohittt0011@gmail.com>
2025-11-09 22:06:09 +0530

    Fix npm integrity issue

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ['https://matify-frontend.vercel.app'],  // your frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }))
 
 // api endpoints
 app.use('/api/user',userRouter)
