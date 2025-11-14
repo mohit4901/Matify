180f8273b97b8a6c388f2dd2932b2aee23445808
Mohit Mudgil <mohittt0011@gmail.com>
2025-11-14 21:18:42 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay} from '../controllers/orderController.js'
+import {placeOrder,  placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyRazorpay} from '../controllers/orderController.js'
 import adminAuth  from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 
@@ -17,7 +17,8 @@ orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
 orderRouter.post('/userorders',authUser,userOrders)
 
 // verify payment
-orderRouter.post('/verifyStripe',authUser, verifyStripe)
+
 orderRouter.post('/verifyRazorpay',authUser, verifyRazorpay)
+
 
 export default orderRouter
